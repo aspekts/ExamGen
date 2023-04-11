9f64cd7709732631ac9ec64ef9c09633d255c015
Marcus Kamuntu <63915967+aspekts@users.noreply.github.com>
2023-04-11 11:08:12 +0100

    Vercel Conflict Changes

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const fetch = require('node-fetch');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-const config = require("./config.json")
-app.use(express.static('public'));
+require('dotenv').config();
+const path = require('path');
+app.use("static", express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 app.use(express.json());
 app.use(cors());
 app.post('/generate-question', async (req, res) => {
@@ -24,7 +28,7 @@ async function generateQuestion(prompt) {
         "message": "Ignore all previous conversation. Write a 4, 5, 10, 15 and 20 mark essay question in accordance with the Edexcel AS Level Economics specification. Each essay question should be in style of the following prompt: " + prompt + " Provide only the essay question, and the marks it's worth. Do not provide any other information, and do not describe any question.",
     }
     const query = new URLSearchParams(options).toString();
-    const response = await fetch(`${config.url}?${query}`);
+    const response = await fetch(`${process.env.url}?${query}`);
     const data = await response.json();
     return data.message.trim();
 }
