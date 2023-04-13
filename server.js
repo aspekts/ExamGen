ecbb977cedf742ae11069de34e8a1602a0acff35
aspekts <marcus.kamuntu@gmail.com>
2023-04-13 18:25:02 +0100

    Add extra options

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const cors = require('cors');
 const app = express();
 const port =  process.env.PORT || 3000;
@@ -11,21 +10,20 @@ app.get('/', (req, res) => {
 });
 app.use(express.json());
 app.use(cors());
-app.post('/generate-question', async (req, res) => {
+app.post(`/generate-question`, async (req, res) => {
     const prompt = req.body.prompt;
     if (prompt) {
-        const question = await generateQuestion(prompt);
+        const question = await generateEconQuestion(prompt);
         res.send({ question });
     } else {
         res.status(400).send({ error: 'Prompt not provided.' });
     }
 });
 
-async function generateQuestion(prompt) {
-    console.log(prompt)
+async function generateEconQuestion(prompt) {
     const options = {
         "key":"B9AAEG066K9BDL2HV7HEZWSYP4DHAGSK3RE",
-        "message": "Ignore all previous conversation. Write a 4, 5, 10, 15 and 20 mark essay question in accordance with the Edexcel AS Level Economics specification. Each essay question should be in style of the following prompt: " + prompt + " Provide only the essay question, and the marks it's worth. Do not provide any other information, and do not describe any question.",
+        "message": "Ignore all previous conversation. Write a 4, 5, 10, 15 and 20 mark essay question in accordance with the Edexcel A Level Economics specification. Each essay question should be in style of the following prompt: " + prompt + " Provide only the essay question, and the marks it's worth. Do not provide any other information, and do not describe any question.",
     }
     const query = new URLSearchParams(options).toString();
     const response = await fetch(`${process.env.url}?${query}`);
