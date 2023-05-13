1b71d3ade863e98c16800b80dd6f30d246212dd6
aspekts <marcus.kamuntu@gmail.com>
2023-05-13 14:08:11 +0100

    Update Prompts Structure

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 
 # QGenie
 
-QGenie is a web application that uses GPT AI Technology to generate exam questions. It is free to use, and is hosted on Netlify.
+QGenie is a web application that uses GPT AI Technology to generate exam questions. It a freemium service, and currently is heavily experimental
 
 ## How it works
 
-QGenie uses GPT AI Technology to generate exam questions. It uses a pre-trained GPT-2 model, and fine-tunes it on a dataset of exam questions. It then uses the fine-tuned model to generate exam questions.
+QGenie uses GPT AI Technology to generate exam questions. It uses a private GPT-model API, and fine-tunes it on a dataset of exam questions. It then uses the fine-tuned model to generate exam questions.
 
 ## How to use
 
-To use QGenie, simply go to [QGenie.netlify.app](QGenie.netlify.app). You will be greeted with a screen that looks like this:
+To use QGenie, simply go to [qgenie.vercel.app](qgenie.vercel.app). You will be greeted with a screen that looks like this:
 
-[Image: image]
+![QGenie Home Page](https://i.imgur.com/WxTCSAe.png)
 
-image1920×1080 101 KB
+Click on "Get Started Now", to log in to your account, where you will be greeted with a screen that looks like this:
 
-You can then select the subject, topic, and difficulty of the exam questions you want to generate. You can also select the number of exam questions you want to generate. Once you have selected all of these options, click the “Generate” button. You will then be greeted with a screen that looks like this:
+![QGenie Login Page](https://i.imgur.com/M3yyLB1.png)
+
+
+You can then select the subject and topic of the exam questions you want to generate. Once you have selected all of these options, click the “Generate” button. You will then be greeted with a screen that looks like this:
+
+![QGenie Generate Page](https://i.imgur.com/jGK5OXz.png)
+
+And that’s it! You can now view your generated exam questions.
--- public/index.js
+++ public/index.js
@@ -2,16 +2,37 @@ const form = document.getElementById('question-form');
 const generateBtn = document.getElementById('generate-btn');
 const questionContainer = document.getElementById('question-container');
 const para = document.createElement('p');
+const promptSelect = document.getElementById('prompt');
+const subPromptSelect = document.getElementById('sub-prompt');
+const nestedSelect = document.getElementById('nested-select');
+import prompts from './subjects.json' assert { type: "json" };
+promptSelect.addEventListener('change', (e) => {
+    const value = e.target.value;
+
+    if (value !== 'none') {
+        subPromptSelect.innerHTML = '';
+        prompts[value].forEach((optionText) => {
+            const option = document.createElement('option');
+            option.value = value;
+            option.innerText = optionText;
+            subPromptSelect.appendChild(option);
+        });
+        nestedSelect.classList.remove('hidden');
+    } else {
+        nestedSelect.classList.add('hidden');
+    }
+});
 if (!questionContainer.innerText) {
     questionContainer.innerText = 'No question generated yet. Try to generate one!';
 }
+
 generateBtn.addEventListener('click', () => {
     const prompt = form.elements.prompt.options[form.elements.prompt.selectedIndex].text;
     const val = form.elements.prompt.options[form.elements.prompt.selectedIndex].value;
     if (prompt && val !== 'none') {
         // Show the loader
         questionContainer.innerHTML = `
-        <img src="./assets/icon.gif" alt="QGenie Logo Anim" class="h-16 w-16"></img>
+            <img src="./assets/icon.gif" alt="QGenie Logo Anim" class="h-16 w-16"></img>
         `;
         fetch('/generate-question', {
             method: 'POST',
@@ -27,16 +48,14 @@ generateBtn.addEventListener('click', () => {
             questionContainer.innerHTML = '';
             para.innerText = question;
             questionContainer.appendChild(para);
-            if(MathJax) {
+            if (MathJax) {
                 MathJax.typeset([questionContainer]);
             }
             // Hide the loader
         })
         .catch(error => console.error(error));
-    } 
-    else {
-        alert('Please select a prompt.');
+    } else {
+        alert('Please select a prompt!');
         return;
     }
-
 });
